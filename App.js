425db322a5710264f9df711a5d4425d7e0d6e0f8
Murphy Hsieh <murphyhsieh@leandev.io>
2020-03-06 17:02:57 +0800

    Finsh wk1 home work

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>學號：110834005</Text>
+      <Text>姓名：謝育哲</Text>
     </View>
   );
 }
